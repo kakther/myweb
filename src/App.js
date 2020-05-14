6514fa80fb33348270f92fea50ded7fbfc818872
Kausar Akther <kausar.akther@gmail.com>
2020-05-14 11:07:41 -0400

    add background image

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Main from './components/main';
 import { Link } from 'react-router-dom';
 
 
+
 class App extends Component {
   render() {
     return (
       
         <div className="demo-big-content">
         <Layout>
-            <Header title="Title" scroll>
+            <Header className="header-color" title="Title" scroll>
                 <Navigation>
                     <Link to="/resume">Resume</Link>
                     <Link to="/aboutme">About Me</Link>
