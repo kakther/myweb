9e93e37ad8f4c1946e9e7d7e37995cf5556a7509
Kausar Akther <kausar.akther@gmail.com>
2020-05-17 13:42:28 -0400

    Finish contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl'
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl'
+import profile_pic from './assets/profile.jpg';
 
 class Contact extends Component {
     render(){
@@ -8,14 +9,42 @@ class Contact extends Component {
                <Grid className="contact-grid">
                    <Cell col={6}>
                        <h2>Kausar Akther</h2>
-                        <img 
-                           src="/Users/kausarakther/Pre_Class_Work/myweb/src/components/assets/profile.jpg" 
-                           alt="profile"
-                           style={{height: '250px'}}
-                        />
+                        <img src={profile_pic} alt="Logo"
+                           style={{height: '260px'}}/>
+                        <p style={{width:'75%', margin:'auto', paddingTop: 'iem'}}>Manual Testing, UAT & Automation Testing experienced QA/Software Tester with expertise of software 
+                        testing tools, techniques, SDLC and various development methodologies. I am now available for a challenging and rewarding position in the testing industry 
+                        to add value to projects as a Manual/Automation/QA/UAT Tester/Analyst.</p>
                    </Cell>
 
-                   <Cell col={6}>half page</Cell>
+                   <Cell col={6}>
+                       <h2>Contact Me</h2>
+                       <hr />
+                        <div className="contact-list">
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '23px', fontFamily: 'Anton'}}>
+                                    <i className="fa fa-phone" aria-hidden="true" />
+                                        (347) 221-5596   
+                                    </ListItemContent>
+                                  </ListItem>
+                                  <ListItem>
+                                    <ListItemContent style={{fontSize: '23px', fontFamily: 'Anton'}}>
+                                    <i className="fa fa-skype" aria-hidden="true" />
+                                        kakther
+                                    </ListItemContent>
+                                  </ListItem>
+                                  <ListItem>
+                                    <ListItemContent style={{fontSize: '23px', fontFamily: 'Anton'}}>
+                                    <i className="fa fa-envelope" aria-hidden="true" />
+                                    kauakther@gmail.com  
+                                    </ListItemContent>
+                                  </ListItem>
+                                    
+                            </List>
+
+                        </div>
+                       
+                   </Cell>
                 </Grid>
            </div> 
         )
