086b62a77558a77ce642728f6badd849ad3b3764
Kausar Akther <kausar.akther@gmail.com>
2020-05-17 12:02:20 -0400

    Update contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
-
+import { Grid, Cell } from 'react-mdl'
 
 class Contact extends Component {
     render(){
         return(
-            <div><h1>Contact Page</h1></div>
+           <div className="contact-body">
+               <Grid className="contact-grid">
+                   <Cell col={6}>
+                       <h2>Kausar Akther</h2>
+                        <img 
+                           src="/Users/kausarakther/Pre_Class_Work/myweb/src/components/assets/profile.jpg" 
+                           alt="profile"
+                           style={{height: '250px'}}
+                        />
+                   </Cell>
+
+                   <Cell col={6}>half page</Cell>
+                </Grid>
+           </div> 
         )
     }
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,9 @@ class Projects extends Component {
   toggleCategories() {
     if(this.state.activeTab  === 0){
         return(
-            // {/* Project #1 */}
+            
+            <div className= "projects-grid">
+                 {/* =========================Project #1 ================================== */}
             <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
                 <CardTitle style={{color:'#fff', height: '176px', background:
                  'url(https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQEkB4gODANjdhxk2MPEt7-6FU_uJECVtm9ZAxM7azz-ZiCfgA&usqp=CAU) center / cover' }}>React Project #1</CardTitle>
@@ -25,46 +27,55 @@ class Projects extends Component {
                  </CardActions>
                  <CardMenu style={{color: '#fff'}}>
                     <IconButton name="share" /> 
-                 </CardMenu>
+                  </CardMenu>
             </Card>
+
+            {/* //  ====================Project #2===============================  */}
+            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <CardTitle style={{color:'#fff', height: '176px', background:
+             'url(https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQEkB4gODANjdhxk2MPEt7-6FU_uJECVtm9ZAxM7azz-ZiCfgA&usqp=CAU) center / cover' }}>React Project #2</CardTitle>
+                 <CardText>
+             Calls are used for describing the functions that you're going to write. In this case, the test is saying, "Okay, I think there's going to be a function called shout, and it should take one argument (it doesn't actually matter what the argument is called, but string, is nice and specific, don't you think?). It should return that argument in all caps.
+                </CardText>
+                <CardActions border> 
+                    <Button colored>Github</Button>
+                    <Button colored>CodePen</Button>
+                    <Button colored>LiveDemo</Button>
+                </CardActions>
+                <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share" /> 
+                </CardMenu>
+            </Card>
+
+
+            {/* // =======================Project #3============================  */}
+            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <CardTitle style={{color:'#fff', height: '176px', background:
+             'url(https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQEkB4gODANjdhxk2MPEt7-6FU_uJECVtm9ZAxM7azz-ZiCfgA&usqp=CAU) center / cover' }}>React Project #3</CardTitle>
+                <CardText>
+             Calls are used for describing the functions that you're going to write. In this case, the test is saying, "Okay, I think there's going to be a function called shout, and it should take one argument (it doesn't actually matter what the argument is called, but string, is nice and specific, don't you think?). It should return that argument in all caps.
+                </CardText>
+                <CardActions border> 
+                <Button colored>Github</Button>
+                <Button colored>CodePen</Button>
+                <Button colored>LiveDemo</Button>
+             </CardActions>
+             <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share" /> 
+             </CardMenu>
+        </Card>
+                
+            </div>
+            
+
             )
     }else if (this.state.activeTab === 1){
         return (
-            // {/* Project #2 */}
-            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color:'#fff', height: '176px', background:
-             'url(https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQEkB4gODANjdhxk2MPEt7-6FU_uJECVtm9ZAxM7azz-ZiCfgA&usqp=CAU) center / cover' }}>React Project #2</CardTitle>
-             <CardText>
-             Calls are used for describing the functions that you're going to write. In this case, the test is saying, "Okay, I think there's going to be a function called shout, and it should take one argument (it doesn't actually matter what the argument is called, but string, is nice and specific, don't you think?). It should return that argument in all caps.
-             </CardText>
-             <CardActions border> 
-                <Button colored>Github</Button>
-                <Button colored>CodePen</Button>
-                <Button colored>LiveDemo</Button>
-             </CardActions>
-             <CardMenu style={{color: '#fff'}}>
-                <IconButton name="share" /> 
-             </CardMenu>
-        </Card>
-        )
+            <div><h1>React Project #1</h1></div>
+            )
     }else if(this.state.activeTab === 2){
         return (
-            // {/* Project #3 */}
-            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color:'#fff', height: '176px', background:
-             'url(https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQEkB4gODANjdhxk2MPEt7-6FU_uJECVtm9ZAxM7azz-ZiCfgA&usqp=CAU) center / cover' }}>React Project #3</CardTitle>
-             <CardText>
-             Calls are used for describing the functions that you're going to write. In this case, the test is saying, "Okay, I think there's going to be a function called shout, and it should take one argument (it doesn't actually matter what the argument is called, but string, is nice and specific, don't you think?). It should return that argument in all caps.
-             </CardText>
-             <CardActions border> 
-                <Button colored>Github</Button>
-                <Button colored>CodePen</Button>
-                <Button colored>LiveDemo</Button>
-             </CardActions>
-             <CardMenu style={{color: '#fff'}}>
-                <IconButton name="share" /> 
-             </CardMenu>
-        </Card>
+             <div><h1>React Project #1</h1></div>
         )
     }
   }
@@ -78,13 +89,13 @@ class Projects extends Component {
                     <Tab>Ruby</Tab>
                 </Tabs>
 
-                <section className="projects-grid">
-                    <Grid className="projects-grid">
+
+                    <Grid>
                         <Cell col={12}>
                             <div className="content">{this.toggleCategories()}</div>
                         </Cell>
                     </Grid>
-                </section>
+               
             </div> 
         )
     }
